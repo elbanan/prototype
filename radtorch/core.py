f73ec5a0e47f9f6152864dfd01164735f8874388
Mohamed Elbanan <elbanan@users.noreply.github.com>
2021-11-07 14:39:14 -0500

    Update core.py

--- radtorch/core.py
+++ radtorch/core.py
@@ -97,7 +97,7 @@ class DICOMDataset():
                 output[k] = DICOMProcessor(root=self.root, ext=self.ext, table=v, class_to_idx = self.class_to_idx, path_col=self.path_col, label_col=self.label_col, transform=self.transforms[k], HU=self.HU, window=self.window, level=self.level).get_loaders(batch_size=batch_size, shuffle=shuffle)
             return output
 
-    def view_batch(self, data='train', figsize = (25,5), rows=2, batch_size=16, shuffle=False, num_images=None):
+    def view_batch(self, data='train', figsize = (25,5), rows=2, batch_size=16, shuffle=True, num_images=None, cmap='gray'):
         loader = DICOMProcessor(root=self.root, ext=self.ext, table=self.data_table[data], class_to_idx = self.class_to_idx, path_col=self.path_col, label_col=self.label_col, transform=self.transforms[data], \
         HU=self.HU, window=self.window, level=self.level).get_loaders(batch_size=batch_size, shuffle=shuffle)
         uidx, images, labels  = (iter(loader)).next()
@@ -121,10 +121,12 @@ class DICOMDataset():
             if images[i].shape[0] == 3:
                 # img = images[i]
                 # img = img / 2 + 0.5
-                ax.imshow(np.transpose(images[i], (1, 2, 0)))
+                out= np.transpose(images[i], (1, 2, 0))
+                ax.imshow((out * 255).astype(np.uint8), cmap=cmap)
+                # ax.imshow(np.transpose(images[i], (1, 2, 0)), cmap=cmap)
 
             elif images[i].shape[0] ==1:
-                ax.imshow(np.squeeze(images[i]), cmap='gray')
+                ax.imshow(np.squeeze(images[i]), cmap=cmap)
 
             ax.set_title(self.classes[labels[i]])
 
